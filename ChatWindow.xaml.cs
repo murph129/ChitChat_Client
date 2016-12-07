ed36c4d54661a6398f20fc4082994fde1513ffcc
alamas7 <alamas@purdue.edu>
2016-12-06 20:06:41 -0500

    Add files via upload

---
+++ ChatWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChitChat
+{
+    /// <summary>
+    /// Interaction logic for ChatWindow.xaml
+    /// </summary>
+    public partial class ChatWindow : Window
+    {
+
+        Window1 display;
+
+        public ChatWindow(Window1 Display)
+        {
+            InitializeComponent();
+            txtInput.KeyDown += new System.Windows.Input.KeyEventHandler(tb_KeyDown);
+            display = Display;
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            //Send text to Server
+            //PsuedoCode: SendServer(Username + input)
+            //Server displays chat
+            //PsuedoCode: txtContent.text = GetServerChat();
+
+            //Only for local use
+            String input = "Username: " + txtInput.Text;
+            txtContent.Text += input;
+            txtContent.Text += Environment.NewLine;
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+
+        private void tb_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.Enter))
+            {
+                btnSend_Click(sender, e);
+            }
+        }
+
+        private void ShowProfile_Click(object sender, RoutedEventArgs e)
+        {
+            display.SwitchtoUser();
+        }
+    }
+}
---
+++ DisplayWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChitChat
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        MainWindow main;
+        SignUpWindow SignUp;
+        ChatWindow Chat;
+        UserProfileWindow User;
+        public Window1()
+        {
+            InitializeComponent();
+            main = new MainWindow(this);
+            SignUp = new SignUpWindow(this);
+            Chat = new ChatWindow(this);
+            User = new UserProfileWindow(this);
+            SwitchtoMain();
+        }
+
+        public void SwitchtoMain()
+        {
+            this.Content = main.Content;
+            this.Title = main.Title;
+        }
+
+        public void SwitchtoSignUp()
+        {
+            this.Content = SignUp.Content;
+            this.Title = SignUp.Title;
+        }
+
+        public void SwitchtoChat()
+        {
+            this.Content = Chat.Content;
+            this.Title = Chat.Title;
+        }
+
+        public void SwitchtoUser()
+        {
+            this.Content = User.Content;
+            this.Title = User.Title;
+        }
+    }
+}
